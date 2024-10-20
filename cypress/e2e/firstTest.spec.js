492f1688aa8fccdd6de287e93819a10425eade2e
Artem Trifonov <artemis.levitt@gmail.com>
2024-10-20 15:23:15 +0200

    Example template

---
+++ cypress/e2e/firstTest.spec.js
@@ -0,0 +1,46 @@
+/// <reference types="cypress" />
+
+describe('First test suit', () => {
+
+  describe('suit section', () => {
+
+    beforeEach('login', () => {
+      // code
+    })
+
+    it('first test', () => {
+      // code
+    })
+
+    it('second test', () => {
+      // code
+    })
+  })
+
+  it('first test', () => {
+    // code
+  })
+
+  it('second test', () => {
+    // code
+  })
+
+  it('third test', () => {
+    // code
+  })
+})
+
+describe('Second test suit', () => {
+
+  it('first test', () => {
+    // code
+  })
+
+  it('second test', () => {
+    // code
+  })
+
+  it('third test', () => {
+    // code
+  })
+})
